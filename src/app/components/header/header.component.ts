cb6b29e5b449f876f5fbeb3f217dc9a91d24e2eb
Ricardo Tadeu Sperlongo <ricardo_tadeu30@hotmail.com>
2023-10-10 16:47:39 -0300

    auth guard, logout

--- src/app/components/header/header.component.ts
+++ src/app/components/header/header.component.ts
@@ -1,4 +1,8 @@
-import { Component } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Component, Input } from '@angular/core';
+import { Router } from '@angular/router';
+import { BehaviorSubject, Observable } from 'rxjs';
+import { User } from 'src/app/models';
 
 @Component({
   selector: 'app-header',
@@ -6,5 +10,22 @@ import { Component } from '@angular/core';
   styleUrls: ['./header.component.scss']
 })
 export class HeaderComponent {
+  private userSubject: BehaviorSubject<User | null>;
+    public user: Observable<User | null>;
+
+    constructor(
+        private router: Router,
+        private http: HttpClient
+    ) {
+        this.userSubject = new BehaviorSubject(JSON.parse(localStorage.getItem('user')!));
+        this.user = this.userSubject.asObservable();
+    }
+
+  logout() {
+    // remove user from local storage and set current user to null
+    localStorage.removeItem('user');
+    this.userSubject.next(null);
+    this.router.navigate(['/login']);
+}
 
 }
